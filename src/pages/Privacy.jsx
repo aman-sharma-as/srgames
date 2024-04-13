b54c9f2fb5fda972d1ee8df266bed2562cb83b65
aman-sharma-as <aman.745500@gmail.com>
2024-04-14 01:51:54 +0530

    Privacy Page Completed

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -357,6 +357,287 @@ const Privacy = () => {
                 </ul>
               </p>
             </div>
+            <div id="toc3" className="description-section">
+              <h3 className="heading">
+                3. WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?
+              </h3>
+              <p className="para1">
+                <span>In Short:</span> We may share information in specific
+                situations described in this section and/or with the following
+                categories of third parties.
+              </p>
+              <p className="para2">
+                Vendors, Consultants, and Other Third-Party Service Providers.
+                We may share your data with third-party vendors, service
+                providers, contractors, or agents ("third parties") who perform
+                services for us or on our behalf and require access to such
+                information to do that work. We have contracts in place with our
+                third parties, which are designed to help safeguard your
+                personal information. This means that they cannot do anything
+                with your personal information unless we have instructed them to
+                do it. They will also not share your personal information with
+                any organization apart from us. They also commit to protect the
+                data they hold on our behalf and to retain it for the period we
+                instruct. The categories of third parties we may share personal
+                information with are as follows:{" "}
+                <ul>
+                  <li>Data Analytics</li>
+                </ul>
+                We also may need to share your personal information in the
+                following situations:
+                <ul>
+                  <li>
+                    <span>Business Transfers.</span> We may share or transfer
+                    your information in connection with, or during negotiations
+                    of, any merger, sale of company assets, financing, or
+                    acquisition of all or a portion of our business to another
+                    company.
+                  </li>
+                  <li>
+                    <span>When we use Google Analytics.</span> We may share your
+                    information with Google Analytics to track and analyze the
+                    use of the Services. The Google Analytics Advertising
+                    Features that we may use include: Google Display Network
+                    Impressions Reporting. To opt out of being tracked by Google
+                    Analytics across the Services, visit{" "}
+                    <a href="https://tools.google.com/dlpage/gaoptout">
+                      https://tools.google.com/dlpage/gaoptout
+                    </a>
+                    . You can opt out of Google Analytics Advertising Features
+                    through Ads Settings and Ad Settings for mobile apps. Other
+                    opt out means include{" "}
+                    <a href="http://optout.networkadvertising.org/">
+                      http://optout.networkadvertising.org/
+                    </a>
+                    and{" "}
+                    <a href="http://www.networkadvertising.org/mobile-choice">
+                      http://www.networkadvertising.org/mobile-choice
+                    </a>
+                    . For more information on the privacy practices of Google,
+                    please visit the{" "}
+                    <a href="https://policies.google.com/privacy">
+                      Google Privacy & Terms page
+                    </a>
+                    .
+                  </li>
+                </ul>
+              </p>
+            </div>
+            <div id="toc4" className="description-section">
+              <h3 className="heading">
+                4. WHAT IS OUR STANCE ON THIRD-PARTY WEBSITES?
+              </h3>
+              <p className="para1">
+                <span>In Short:</span> We are not responsible for the safety of
+                any information that you share with third parties that we may
+                link to or who advertise on our Services, but are not affiliated
+                with, our Services.
+              </p>
+              <p className="para2">
+                The Services may link to third-party websites, online services,
+                or mobile applications and/or contain advertisements from third
+                parties that are not affiliated with us and which may link to
+                other websites, services, or applications. Accordingly, we do
+                not make any guarantee regarding any such third parties, and we
+                will not be liable for any loss or damage caused by the use of
+                such third-party websites, services, or applications. The
+                inclusion of a link towards a third-party website, service, or
+                application does not imply an endorsement by us. We cannot
+                guarantee the safety and privacy of data you provide to any
+                third parties. Any data collected by third parties is not
+                covered by this privacy notice. We are not responsible for the
+                content or privacy and security practices and policies of any
+                third parties, including other websites, services, or
+                applications that may be linked to or from the Services. You
+                should review the policies of such third parties and contact
+                them directly to respond to your questions.
+              </p>
+            </div>
+            <div id="toc5" className="description-section">
+              <h3 className="heading">
+                5. HOW DO WE HANDLE YOUR SOCIAL LOGINS?
+              </h3>
+              <p className="para1">
+                <span>In Short:</span> If you choose to register or log in to
+                our Services using a social media account, we may have access to
+                certain information about you.
+              </p>
+              <p className="para2">
+                Our Services offer you the ability to register and log in using
+                your third-party social media account details (like your
+                Facebook or Twitter logins). Where you choose to do this, we
+                will receive certain profile information about you from your
+                social media provider. The profile information we receive may
+                vary depending on the social media provider concerned, but will
+                often include your name, email address, friends list, and
+                profile picture, as well as other information you choose to make
+                public on such a social media platform.
+              </p>
+              <p className="para3">
+                We will use the information we receive only for the purposes
+                that are described in this privacy notice or that are otherwise
+                made clear to you on the relevant Services. Please note that we
+                do not control, and are not responsible for, other uses of your
+                personal information by your third-party social media provider.
+                We recommend that you review their privacy notice to understand
+                how they collect, use, and share your personal information, and
+                how you can set your privacy preferences on their sites and
+                apps.
+              </p>
+            </div>
+            <div id="toc6" className="description-section">
+              <h3 className="heading">
+                6. HOW LONG DO WE KEEP YOUR INFORMATION?
+              </h3>
+              <p className="para1">
+                <span>In Short:</span> We keep your information for as long as
+                necessary to fulfill the purposes outlined in this privacy
+                notice unless otherwise required by law.
+              </p>
+              <p className="para2">
+                We will only keep your personal information for as long as it is
+                necessary for the purposes set out in this privacy notice,
+                unless a longer retention period is required or permitted by law
+                (such as tax, accounting, or other legal requirements). No
+                purpose in this notice will require us keeping your personal
+                information for longer than 1 year.
+              </p>
+              <p className="para3">
+                When we have no ongoing legitimate business need to process your
+                personal information, we will either delete or anonymize such
+                information, or, if this is not possible (for example, because
+                your personal information has been stored in backup archives),
+                then we will securely store your personal information and
+                isolate it from any further processing until deletion is
+                possible.
+              </p>
+            </div>
+            <div id="toc7" className="description-section">
+              <h3 className="heading">
+                7. HOW DO WE KEEP YOUR INFORMATION SAFE?
+              </h3>
+              <p className="para1">
+                <span>In Short:</span> We aim to protect your personal
+                information through a system of organizational and technical
+                security measures.
+              </p>
+              <p className="para2">
+                We have implemented appropriate and reasonable technical and
+                organizational security measures designed to protect the
+                security of any personal information we process. However,
+                despite our safeguards and efforts to secure your information,
+                no electronic transmission over the Internet or information
+                storage technology can be guaranteed to be 100% secure, so we
+                cannot promise or guarantee that hackers, cybercriminals, or
+                other unauthorized third parties will not be able to defeat our
+                security and improperly collect, access, steal, or modify your
+                information. Although we will do our best to protect your
+                personal information, transmission of personal information to
+                and from our Services is at your own risk. You should only
+                access the Services within a secure environment.
+              </p>
+            </div>
+            <div id="toc8" className="description-section">
+              <h3 className="heading">8. WHAT ARE YOUR PRIVACY RIGHTS?</h3>
+              <p className="para1">
+                <span>In Short:</span> You may review, change, or terminate your
+                account at any time.
+              </p>
+              <p className="para2">
+                <span>Withdrawing your consent:</span>If we are relying on your
+                consent to process your personal information, which may be
+                express and/or implied consent depending on the applicable law,
+                you have the right to withdraw your consent at any time. You can
+                withdraw your consent at any time by contacting us by using the
+                contact details provided in the section "HOW CAN YOU CONTACT US
+                ABOUT THIS NOTICE?" below.
+              </p>
+              <p className="para3">
+                However, please note that this will not affect the lawfulness of
+                the processing before its withdrawal nor, when applicable law
+                allows, will it affect the processing of your personal
+                information conducted in reliance on lawful processing grounds
+                other than consent.
+              </p>
+              <p className="para4">
+                If you have questions or comments about your privacy rights, you
+                may email us at{" "}
+                <a href="mailto:support@srgamestudio.com">
+                  support@srgamestudio.com
+                </a>
+                .
+              </p>
+            </div>
+            <div id="toc9" className="description-section">
+              <h3 className="heading">9. CONTROLS FOR DO-NOT-TRACK FEATURES</h3>
+              <p className="para2">
+                Most web browsers and some mobile operating systems and mobile
+                applications include a Do-Not-Track ("DNT") feature or setting
+                you can activate to signal your privacy preference not to have
+                data about your online browsing activities monitored and
+                collected. At this stage no uniform technology standard for
+                recognizing and implementing DNT signals has been finalized. As
+                such, we do not currently respond to DNT browser signals or any
+                other mechanism that automatically communicates your choice not
+                to be tracked online. If a standard for online tracking is
+                adopted that we must follow in the future, we will inform you
+                about that practice in a revised version of this privacy notice.
+              </p>
+            </div>
+            <div id="toc10" className="description-section">
+              <h3 className="heading">
+                10. DO WE MAKE UPDATES TO THIS NOTICE?
+              </h3>
+              <p className="para1">
+                <span>In Short:</span> Yes, we will update this notice as
+                necessary to stay compliant with relevant laws.
+              </p>
+              <p className="para2">
+                We may update this privacy notice from time to time. The updated
+                version will be indicated by an updated "Revised" date and the
+                updated version will be effective as soon as it is accessible.
+                If we make material changes to this privacy notice, we may
+                notify you either by prominently posting a notice of such
+                changes or by directly sending you a notification. We encourage
+                you to review this privacy notice frequently to be informed of
+                how we are protecting your information.
+              </p>
+            </div>
+            <div id="toc11" className="description-section">
+              <h3 className="heading">
+                11. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?
+              </h3>
+              <p className="para2">
+                If you have questions or comments about this notice, you may
+                email us at{" "}
+                <a href="mailto:support@srgamestudio.com">
+                  support@srgamestudio.com
+                </a>{" "}
+                or contact us by post at:
+              </p>
+              <p className="para3">Shi Systems Inc.</p>
+              <p className="para4">XXX-XXX-ADDRESS</p>
+              <p className="para5">
+                XXX-XXX-CITY, XXX-XXX-STATE XXX-XXX-ZIPCODE
+              </p>
+              <p className="para6">India</p>
+            </div>
+            <div id="toc12" className="description-section">
+              <h3 className="heading">
+                12. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT
+                FROM YOU?
+              </h3>
+              <p className="para2">
+                You have the right to request access to the personal information
+                we collect from you, change that information, or delete it. To
+                request to review, update, or delete your personal information,
+                please visit:{" "}
+                <a href="http://www.srgamestudio.com">
+                  http://www.srgamestudio.com
+                </a>
+                .
+              </p>
+            </div>
           </section>
         </div>
       </div>
